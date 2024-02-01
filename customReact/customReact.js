e82097cf6acfa91c26edc740be4bdaa17a0314d5
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-01 09:00:39 +0530

    customReact

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -1,22 +1,30 @@
 function customRender(reactElement, container){
+    // const domElement = document.createElement(reactElement.type);
+    // domElement.innerHTML = reactElement.children;
+    // domElement.setAttribute('href', reactElement.props.href);
+    // domElement.setAttribute('target', reactElement.props.target);
+
+    // container.appendChild(domElement);
+
     const domElement = document.createElement(reactElement.type);
-    domElement.innerHTML = reactElement.children
-    domElement.setAttribute = ('href', reactElement.props.href)
-    domElement.setAttribute = ('target', reactElement.props.target)
-
+    domElement.innerHTML = reactElement.children;
+    for (const prop in reactElement.props){
+        if(prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
     container.appendChild(domElement)
-}
-
+} 
 
 const reactElement = {
-    type: "a",
-    props: {
-        href: 'https://google.com',
-        target: '_blank'
+    type: 'a',
+    props : {
+        href: 'https://facebook.github.io/react/',
+        target: '_blank',
     },
-    children: "Click me to visit Google"
+    children: "Click me to visit"
 }
 
-const mainContainer = document.getElementById("root");
 
-customRender(reactElement, mainContainer)
+const mainContainer = document.getElementById('root');
+
+customRender(reactElement, mainContainer);
