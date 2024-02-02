40bc716474531c1f5575f0ca04d5be6e740b88bd
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-02 22:14:09 +0530

    02/02/2024

--- Projects/02tailwindprops/src/App.jsx
+++ Projects/02tailwindprops/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Card from './components/Card';
-
+import Cart from './components/Cart';
 function App() {
 
   let obj = {
@@ -13,6 +13,7 @@ function App() {
       <h1 className="bg-green-400 text-black mb-4 p-3 rounded-md">Hello world!</h1>
       <Card username="Abhi" myObj={obj} btnText="Click Me" />
       <Card username="Suraj" myObj={obj} btnText="Visit Me" />
+      <Cart productName="Nike" price= "₹45,299"/>
     </>
   );
 }
