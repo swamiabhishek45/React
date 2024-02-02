40121b7911abdddd8c25894a94b58f2b607f68b3
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-02 09:35:06 +0530

    changes

--- 01counter/src/App.jsx
+++ 01counter/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let [counter, setCounter] = useState(15);
+  let [counter, setCounter] = useState(0);
 
   // let counter = 5;
   const addValue = () => {
     // console.log("Clickd", counter);
     // counter = counter + 1;
-    setCounter(counter + 1);
+    if(counter < 20){
+      setCounter(counter + 1);
+    }
+      
   };
 
   const removeValue = () => {
