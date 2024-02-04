ec2c7dd255f2226283dd9f1ff70b9cea62e3edf0
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-04 12:10:57 +0530

    phase 1 of custom hook done

--- Projects/06currencyConvertor/src/App.jsx
+++ Projects/06currencyConvertor/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-     <h1 className='text-3xl'>Its working</h1>
+     <h1 className='text-3xl'>Currency Convertor</h1>
     </>
   )
 }
