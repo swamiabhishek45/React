326fc251d68d32dea62d614a7e2b741980eb8c16
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-04 13:28:56 +0530

    phase 2 basic done

--- Projects/06currencyConvertor/src/App.jsx
+++ Projects/06currencyConvertor/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import InputBox from './components/InputBox'
 
 function App() {
   
   return (
     <>
      <h1 className='text-3xl'>Currency Convertor</h1>
+<InputBox />
     </>
   )
 }
