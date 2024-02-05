5168a8a143b76f46513147bcb30289dfea5014d7
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-05 23:24:18 +0530

    05/02/2024

--- Projects/07reactRouter/src/components/Github/Github.jsx
+++ Projects/07reactRouter/src/components/Github/Github.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 function Github() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://api.github.com/users/swamiabhishek45")
-      .then(response => response.json())
-      .then((data) => {
-        setData(data);
-      });
-  }, []);
+
+  const data = useLoaderData();
+
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   fetch("https://api.github.com/users/swamiabhishek45")
+  //     .then(response => response.json())
+  //     .then((data) => {
+  //       setData(data);
+  //     });
+  // }, []);
   return (
     <>
     <div className="bg-gray-700 text-white p-4 text-3xl text-center flex flex-col justify-center items-center gap-4">
@@ -20,3 +24,9 @@ function Github() {
 }
 
 export default Github;
+
+export const githubInfoLoader = async () =>{
+  const response = await fetch("https://api.github.com/users/swamiabhishek45")
+  return response.json();
+
+}
