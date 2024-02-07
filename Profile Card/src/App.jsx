82dcf0b305ee9a0662d1271a8d7f26abf7a92d3b
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-07 23:46:04 +0530

    07/02/2024

--- Profile Card/src/App.jsx
+++ Profile Card/src/App.jsx
@@ -5,22 +5,29 @@ function App() {
 
   return (
     <>
-      <div className=" border-black border-4 flex flex-col justify-center items-center">
+      <div className=" p-2 w-80 border-black border-4 flex flex-col justify-center items-center">
         <img
           src="https://img.freepik.com/premium-photo/cute-handsome-anime-boy-blue-light_675932-435.jpg"
           alt=""
           width={122}
+          className='bg-cover bg-center rounded-full mb-8 shadow-lg m-3
+          '
         />
         <h1 className="text-3xl font-bold">Abhishek Swami</h1>
-        <p>
+        <p className='text-left'>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate
           deserunt officia nesciunt rem natus quidem tempora enim ipsum,
           praesentium dicta officiis itaque est libero ut? Quos sequi iusto ut
           itaque.
         </p>
+      <div className='flex gap-2 flex-wrap justify-start'>
+        <Bubble name="HTML+CSS" color="orange" />
+        <Bubble name="JavaScript" color="yellow" />
+        <Bubble name="Web Design" color="lightgreen" />
+        <Bubble name="Git and GitHub" color="red" />
+        <Bubble name="React" color="lightblue" />
+        <Bubble name="Tailwind CSS" color="blue" />
       </div>
-      <div>
-        <Bubble name="HTML+CSS" color="blue" />
       </div>
     </>
   );
--- Profile Card/src/Bubble.jsx
+++ Profile Card/src/Bubble.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Bubble(props) {
   return (
-    <div>
-        <h1 className='w-auto' style={{backgroundColor: props.color}}>{props.name}</h1>
+    <div className=''>
+        <h1 className='mt-2 w-auto px-2 font-bold rounded-md' style={{backgroundColor: props.color}}>{props.name}</h1>
     </div>
   )
 }
