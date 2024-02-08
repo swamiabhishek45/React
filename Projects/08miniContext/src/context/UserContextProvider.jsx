94d127d848d0752dfcd25619d7d0d598e2eb240e
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-08 19:24:21 +0530

    context api project

--- Projects/08miniContext/src/context/UserContextProvider.jsx
+++ Projects/08miniContext/src/context/UserContextProvider.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import UserContext from "./UserContext";
 
-const UserContextProvider = ({ children }) => {
-  const [user, setUser] = React.useState(null);
-  return (
-    <UserContext.Provider value={{ user, setUser }}>
-      {children}
-    </UserContext.Provider>
-  );
-};
-export default UserContextProvider;
+const UserContextProvider = ({children}) => {
 
+  const [user, setUser] = useState(null);
+  return (
+    <UserContext.Provider value={{user, setUser}}>
+    {children}
+    </UserContext.Provider>
+  )
+}
+
+export default UserContextProvider;
