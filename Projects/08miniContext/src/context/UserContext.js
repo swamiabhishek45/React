30bc921e96a5b2c5a3c1c3acb17877f7af0abbc0
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-11 09:15:26 +0530

    add one line for three lines

--- Projects/08miniContext/src/context/UserContext.js
+++ Projects/08miniContext/src/context/UserContext.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const UserContext = React.createContext();
 
 export default UserContext;
+
+// This one line is enough
+// export const UserContext = React.createContext();
--- Projects/08miniContext/src/context/UserContextProvider.jsx
+++ Projects/08miniContext/src/context/UserContextProvider.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import UserContext from "./UserContext";
+
+// const UserContextProvider = ({children}) => {
+
+//   const [user, setUser] = useState(null);
+//   return (
+//     <UserContext.Provider value={{user, setUser}}>
+//     {children}
+//     </UserContext.Provider>
+//   )
+// }
+
+// export default UserContextProvider;
+
+
+
+import React from "react";
 import UserContext from "./UserContext";
 
 const UserContextProvider = ({children}) => {
-
-  const [user, setUser] = useState(null);
   return (
-    <UserContext.Provider value={{user, setUser}}>
+    <UserContext.Provider>
     {children}
     </UserContext.Provider>
   )
+
 }
 
 export default UserContextProvider;
