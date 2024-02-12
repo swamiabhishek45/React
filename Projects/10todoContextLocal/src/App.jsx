0c7319c6a152564d1610870c5d877f67bad96b53
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-12 18:11:45 +0530

    basic context functionality completed

--- Projects/10todoContextLocal/src/App.jsx
+++ Projects/10todoContextLocal/src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
-import './App.css'
-import { TodoProvider } from './contexts';
+import { useState } from "react";
+import "./App.css";
+import { TodoProvider } from "./contexts";
 
 function App() {
+  const [todos, setTodos] = useState([]);
 
-  
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
 
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((prevTodo) => prevTodo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
   return (
-    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
@@ -23,4 +47,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
