91ff7306dcbecc17477a79fd0818602dfc54a6e3
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-14 08:10:04 +0530

    done

--- Projects/10todoContextLocal/src/components/TodoForm.jsx
+++ Projects/10todoContextLocal/src/components/TodoForm.jsx
@@ -11,7 +11,7 @@ function TodoForm() {
 
       if(!todo) return 
 
-      addTodo({ todo, completed: false})
+      addTodo({id: Date.now(), todo, completed: false})
       setTodo("");
     }
   return (
