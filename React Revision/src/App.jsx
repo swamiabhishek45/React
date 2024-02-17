d2bbad62b992de562ed4d6e3618c8de121cce247
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-17 19:56:23 +0530

    changes

--- React Revision/src/App.jsx
+++ React Revision/src/App.jsx
@@ -1,56 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { TodoProvider } from "./contexts";
+import React from 'react'
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  const addTodo = (todo) => {
-    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
-  };
-
-  const updateTodo = (id, todo) => {
-    setTodos((prevTodos) =>
-      prevTodos.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
-    );
-  };
-
-  const removeTodo = (id) => {
-    setTodos((prev)=> prev.filter(prevTodo => prevTodo.id !== id))
-  };
-
-  const toggleTodo = (id) => {
-    setTodos((prev) => prev.map(prevTodo => prevTodo.id === id ? {...prevTodo, isCompleted : !prevTodo.isCompleted} : prevTodo ))
-  };
-
-
-  useEffect(() => {
-    const todos = JSON.parse(localStorage.getItem("todos"));
-    if(todos && todos.length > 0) {
-      setTodos(todos);
-    } 
-  },[]);
-
-  useEffect(() => {
-    localStorage.setItem("todos",JSON.stringify(todos))
-  },[todos]);
-
   return (
-    <TodoProvider
-      value={{ todos, addTodo, updateTodo, removeTodo, toggleTodo }}
-    >
-      <div className="bg-[#172842] min-h-screen py-8">
-        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-            Manage Your Todos
-          </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
-          <div className="flex flex-wrap gap-y-3">
-            {/*Loop and Add TodoItem here */}
-          </div>
-        </div>
-      </div>
-    </TodoProvider>
-  );
+    <div>
+      <h1>mamala thik hai</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
