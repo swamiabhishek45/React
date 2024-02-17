7b93c48e28c2fbb5b5824d1ac760a7fc353976fa
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-17 19:37:24 +0530

    redux practice start

--- Projects/10todoContextLocal/src/components/TodoForm.jsx
+++ Projects/10todoContextLocal/src/components/TodoForm.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useTodo } from '../contexts';
 
 function TodoForm() {
+
+    const [todo, setTodo] = useState("");
+    const {addTodo} = useTodo();
+
+    const add = (e) => {
+      e.preventDefault();
+
+      if(!todo) return 
+
+      addTodo({id: Date.now(), todo, completed: false})
+      setTodo("");
+    }
   return (
-    <div>
-      
-    </div>
-  )
+    <form onSubmit={add} className="flex">
+      <input
+        type="text"
+        placeholder="Enter Your Task"
+        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        value={todo}
+        onChange={(e) => {
+            setTodo(e.target.value);
+        }}
+      />
+      <button
+        type="submit"
+        className="rounded-r-lg px-3 py-1 bg-green-600 hover:bg-green-700 text-white shrink-0"
+      >
+        Add
+      </button>
+    </form>
+  );
 }
 
-export default TodoForm
+export default TodoForm;
