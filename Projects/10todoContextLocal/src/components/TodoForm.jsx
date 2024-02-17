4507ad6ea28f9f8db8afe8d9175cb519e9795a8b
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-17 19:32:34 +0530

    done with context api

--- Projects/10todoContextLocal/src/components/TodoForm.jsx
+++ Projects/10todoContextLocal/src/components/TodoForm.jsx
@@ -1,38 +1,11 @@
-import React, { useState } from 'react'
-import { useTodo } from '../contexts';
+import React from 'react'
 
 function TodoForm() {
-
-    const [todo, setTodo] = useState("");
-    const {addTodo} = useTodo();
-
-    const add = (e) => {
-      e.preventDefault();
-
-      if(!todo) return 
-
-      addTodo({id: Date.now(), todo, completed: false})
-      setTodo("");
-    }
   return (
-    <form onSubmit={add} className="flex">
-      <input
-        type="text"
-        placeholder="Enter Your Task"
-        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
-        value={todo}
-        onChange={(e) => {
-            setTodo(e.target.value);
-        }}
-      />
-      <button
-        type="submit"
-        className="rounded-r-lg px-3 py-1 bg-green-600 hover:bg-green-700 text-white shrink-0"
-      >
-        Add
-      </button>
-    </form>
-  );
+    <div>
+      
+    </div>
+  )
 }
 
-export default TodoForm;
+export default TodoForm
