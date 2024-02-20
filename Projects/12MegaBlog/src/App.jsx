4547314701bebfcca65d20ae84eba079d2710eb3
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-20 13:08:59 +0530

    update

--- Projects/12MegaBlog/src/App.jsx
+++ Projects/12MegaBlog/src/App.jsx
@@ -1,29 +1,41 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import authService from './appwrite/auth'
-import { login, logout } from './store/authSlice'
-import './App.css'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
+import { Outlet } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  useEffect(()=> {
-    authService.getCurrentUser()
-    .then((userData) => {
-      if(userData) {
-        dispatch(login({userData}))
-      } else {
-        dispatch(logout())
-      }
-    })
-    // .catch((error) => error)
-    .finally(() => setLoading(false))
-  },[])
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      // .catch((error) => error)
+      .finally(() => setLoading(false));
+  }, []);
   return !loading ? (
-    <div className='min-h-screen'>hi</div>
-  ) : null
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          Todo
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
