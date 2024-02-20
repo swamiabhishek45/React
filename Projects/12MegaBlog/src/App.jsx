db42925960e59ae7cfaaf9cf50350b780de286b6
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-20 09:59:38 +0530

    componenets created

--- Projects/12MegaBlog/src/App.jsx
+++ Projects/12MegaBlog/src/App.jsx
@@ -1,15 +1,29 @@
-
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-    <h1> Mega blog start</h1>
-    </>
-  )
+  useEffect(()=> {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    // .catch((error) => error)
+    .finally(() => setLoading(false))
+  },[])
+  return !loading ? (
+    <div className='min-h-screen'>hi</div>
+  ) : null
 }
 
 export default App
