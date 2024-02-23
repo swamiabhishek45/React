4c0c43f5a27965d8f6a03d4cab77e3255fe9a77e
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-23 09:15:54 +0530

    react hook form

---
+++ 04ReactHookForm/src/App.jsx
@@ -0,0 +1,69 @@
+import "./App.css";
+import { useForm } from "react-hook-form";
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const delay = (d) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, d * 1000);
+    });
+  };
+
+  const onSubmit = async (data) => {
+    await delay(2); // simulating network delay
+    console.log(data);
+  };
+  return (
+    <>
+      {isSubmitting && <p>Loading...</p>}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {errors.username && (
+          <p className="text-red-500">{errors.username.message}</p>
+        )}
+        <input
+          type="text"
+          placeholder="Enter your name"
+          {...register("username", {
+            required: true,
+            minLength: {
+              value: 3,
+              message: "Min length must be 3",
+            },
+          })}
+        />
+        <br />
+        <input
+          className="mt-2"
+          type="password"
+          placeholder="Enter your password"
+          {...register("password", {
+            required: true,
+            minLength: {
+              value: 8,
+              message: "Passlength must be 8",
+            },
+          })}
+        />
+        {errors.password && (
+          <p className="bg-red-600">{errors.password.message} </p>
+        )}
+        <br />
+        <input
+          className="bg-blue-500 rounded-md p-2 mt-2"
+          disabled={isSubmitting}
+          type="submit"
+          value="submit"
+        />
+      </form>
+    </>
+  );
+}
+
+export default App;
