006ee0eac5c85c90516f272b8cb6eb2a39ee5441
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-27 00:11:25 +0530

    docs new project

---
+++ Jonas Project/docs/src/App.jsx
@@ -0,0 +1,14 @@
+import Background from "./components/Background";
+import ForeGround from "./components/ForeGround";
+
+function App() {
+
+  return (
+    <>
+     <Background />
+     <ForeGround />
+    </>
+  );
+}
+
+export default App
---
+++ Jonas Project/docs/src/components/ForeGround.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ForeGround() {
+  return (
+    <>
+      <div className="w-full"></div>
+    </>
+  )
+}
+
+export default ForeGround
