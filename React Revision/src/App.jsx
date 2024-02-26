3149a95cb53b8a727a8efaf0958a238ed809517a
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-26 23:06:51 +0530

    change

--- React Revision/src/App.jsx
+++ React Revision/src/App.jsx
@@ -2,31 +2,10 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  const onIncrease = () => {
-    setCounter(counter + 1);
-  };
-  const onDecrease = () => {
-    setCounter(counter - 1);
-  };
-  
   return (
-    <div className="border-2 mx-auto  w-3/12 p-6 h-44 rounded-3xl">
-      <div className="text-6xl font-bold text-center">{counter}</div>
-      <div className="flex justify-between">
-        <div className="border-2 w-10 h-10 items-center flex justify-center mt-6 rounded-full">
-          <button onClick={onDecrease} className="text-4xl">
-            -
-          </button>
-        </div>
-        <div className="border-2 w-10 h-10 items-center flex justify-center mt-6 rounded-full">
-          <button onClick={onIncrease} className="text-4xl">
-            +
-          </button>
-        </div>
-      </div>
-    </div>
+   <>
+   
+   </>
   );
 }
 
