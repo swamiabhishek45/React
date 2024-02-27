e96c0e49ec28935ce1a66b99d67fd58edb386520
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-27 21:05:56 +0530

    push

--- Jonas Project/docs/src/components/Card.jsx
+++ Jonas Project/docs/src/components/Card.jsx
@@ -1,25 +1,37 @@
 import React from "react";
 import { FaRegFileAlt } from "react-icons/fa";
 import { HiOutlineDownload } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion";
 
-function Card() {
+function Card({ data, reference }) {
   return (
     <>
-      <div className="relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
+      <motion.div drag dragConstraints={reference} className="relative flex-shrink-0 w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
         <FaRegFileAlt />
-        <p className="text-sm mt-5 leading-tight font-semibold">
-          Lorem, ipsum dolor sit amet consectetur adipisicing.
-        </p>
+        <p className="text-sm mt-5 leading-tight font-semibold">{data.desc}</p>
         <div className="footer absolute bottom-0  w-full   left-0">
           <div className="flex items-center justify-between mb-2 px-8 py-3">
-            <h1>.4mb</h1>
+            <h1>{data.filesize}</h1>
             <span className="h-8 w-8 bg-zinc-200 rounded-full flex items-center justify-center">
-              <HiOutlineDownload size="1em" color="#000" />
+              {data.close ? (
+                <IoClose size="1em" color="#000" />
+              ) : (
+                <HiOutlineDownload size="1em" color="#000" />
+              )}
             </span>
           </div>
-          <div className="tag w-full h-10 bg-sky-200"></div>
+          {data.tagDetails.isOpen && (
+            <div
+              className={`tag w-full h-10 py-4 flex justify-center items-center ${data.tagDetails.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"}`}
+            >
+              <h3 className="text-sm font-semibold">
+                {data.tagDetails.tagTitle}
+              </h3>
+            </div>
+          )}
         </div>
-      </div>
+      </motion.div>
     </>
   );
 }
--- Jonas Project/docs/src/components/ForeGround.jsx
+++ Jonas Project/docs/src/components/ForeGround.jsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Card from "./Card";
 
+
 function ForeGround() {
+  const ref = useRef();
+  const data = [
+    //desc, filesize, close, tagDetails
+
+    {
+      desc: "lorem ispum doloe sit amer sonnk seklrhoi askndfjo nsdfiho",
+      filesize: ".9mb",
+      close: false,
+      tagDetails: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+    {
+      desc: "lorem ispum doloe sit amer sonnk seklrhoi askndfjo nsdfiho",
+      filesize: ".9mb",
+      close: true,
+      tagDetails: {
+        isOpen: true,
+        tagTitle: "Upload",
+        tagColor: "blue",
+      },
+    },
+    {
+      desc: "lorem ispum doloe sit amer sonnk seklrhoi askndfjo nsdfiho",
+      filesize: ".9mb",
+      close: true,
+      tagDetails: {
+        isOpen: false,
+        tagTitle: "Download Now",
+        tagColor: "green",
+      },
+    },
+  ];
+  useState();
   return (
     <>
-      <div className="fixed z-[3] top-0 left-0 w-full h-full">
-        <Card />
+      <div ref={ref} className="fixed p-5 z-[3] top-0 left-0 w-full h-full flex gap-10 flex-wrap">
+        {data.map((item, index) => (
+          <Card data={item} reference={ref} />
+          // console.log(item)
+        ))}
       </div>
     </>
   );
